f9fbc3b81528f9def1581dcd30e95db7defb5fb6
Hossam-Elsonbaty <armaggg3@gmail.com>
2023-11-18 16:17:33 +0200

    new cart page

--- e-commerce/src/components/myCart.jsx
+++ e-commerce/src/components/myCart.jsx
@@ -4,10 +4,10 @@ import Footer from './footer';
 import image from '../NEW QC/New folder/Pro2.jpg';
 import image2 from '../NEW QC/New folder/Pro3.jpg';
 import image3 from '../NEW QC/New folder/wordpress-shopping-cart-plugins.webp';
-import { InputNumber } from 'primereact/inputnumber';
-import axios from 'axios';
+import { InputTextarea } from "primereact/inputtextarea";import axios from 'axios';
 function MyCart() {
   const [value2, setValue2] = useState(10.50);
+  const [value, setValue] = useState("");
   const [cartItems,setCartItems] = useState();
   useEffect(()=>{
     getCartItems()
@@ -24,7 +24,94 @@ function MyCart() {
     <>
       <div className='cart-container'>
         <Navbar/> 
-        <div className='landing-image' >
+        <div className='main-content'>
+          <section className='items-container'>
+            <div className="cart-labels">
+              <span className='label'>العربة</span>
+              <span className='label'>الكمية</span>
+              <span className='label'>المجموع الكلي</span>
+              <span className='label'>ازالة</span>
+            </div>
+            <div className='item-container'>
+              <div className='image-name-disc'>
+                <img src={image} alt=""/>
+                <div className='name-disc'>
+                  <span className='item-name'>قشطوطه باللبن</span>
+                  <span>كيكه اسفنجيه مغموره بالحليب </span>
+                </div>
+              </div>
+              <div className='quantity-div'>
+                <div className='calcDiv-container'>
+                  <div className='calcDiv'>
+                    <button type='button'  className='calc-btn'><i className='pi pi-angle-up'></i></button>
+                    <button type='button'  className='calc-btn'><i className='pi pi-angle-down'></i></button>
+                  </div>
+                  <div className='count'>2</div>
+                </div>
+              </div>
+              <div className="totalPrice">
+                <span className='total'>82</span>
+              </div>
+              <div className="options">
+                <button className="delete-item">
+                  <i className='pi pi-trash'></i>
+                </button>
+              </div>
+            </div>
+            <div className='item-container'>
+              <div className='image-name-disc'>
+                <img src={image2} alt=""/>
+                <div className='name-disc'>
+                  <span className='item-name'>قشطوطه باللبن</span>
+                  <span>كيكه اسفنجيه مغموره بالحليب </span>
+                </div>
+              </div>
+              <div className='quantity-div'>
+                <div className='calcDiv-container'>
+                  <div className='calcDiv'>
+                    <button type='button'  className='calc-btn'><i className='pi pi-angle-up'></i></button>
+                    <button type='button'  className='calc-btn'><i className='pi pi-angle-down'></i></button>
+                  </div>
+                  <div className='count'>2</div>
+                </div>
+              </div>
+              <div className="totalPrice">
+                <span className='total'>82</span>
+              </div>
+              <div className="options">
+                <button className="delete-item">
+                  <i className='pi pi-trash'></i>
+                </button>
+              </div>
+            </div>
+          </section>
+          <section className='order-details'>
+            <div className="top-section">
+              <h3>طلب خاص</h3>
+              <InputTextarea autoResize style={{border:'none'}} value={value} onChange={(e) => setValue(e.target.value)} rows={3} cols={10} />
+            </div>
+            <div className="bottom-section">
+              <div className='subtotal'>
+                <div className='col'>
+                  <span>المجموع الفرعي:</span>
+                  <span>EGP 100.88</span>
+                </div>
+                <div className='col'>
+                  <span>الضريبة المضافة:</span>
+                  <span>EGP 14.12</span>
+                </div>
+              </div>
+              <div className="total-order">
+                <div className='col'>
+                  <span className='order-total-price'>المجموع الكلي</span>
+                  <span className='order-totalPrice'>EGP  115</span>
+                </div>
+              </div>
+            </div>
+          <button className='checkout'>متابعة عملية الشراء</button>
+          </section>
+        </div>
+        {/* <div className='landing-image' >
           <img src={image3} alt="" />
         </div>
         <section className='top-col'>
@@ -347,8 +434,10 @@ function MyCart() {
             <span>سعر التوصيل : <span>$240</span></span>
             <button className='checkOut-btn'>متابعة عملية الشراء</button>
           </div>
-        </section>
-      <Footer/>
+        </section> */}
+        <div className="footer2" style={{width:'100%'}}>
+          <Footer/>
+        </div>
       </div>
     </>
   )
