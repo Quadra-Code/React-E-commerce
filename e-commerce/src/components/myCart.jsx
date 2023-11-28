89260e3d7a876c43b1a98911e3a1556e4c1eae9c
Mostafa Elsonbaty <armaggg3@gmail.com>
2023-11-28 21:36:05 +0200

    cart

--- e-commerce/src/components/myCart.jsx
+++ e-commerce/src/components/myCart.jsx
@@ -10,17 +10,49 @@ function MyCart() {
   const [value2, setValue2] = useState(10.50);
   const [value, setValue] = useState("");
   const [cartItems,setCartItems] = useState();
+  const [TotalPrice, setTotalPrice] = useState(0);
+
   useEffect(()=>{
     getCartItems()
-  },[])
+    handleTotalPrice()
+  },[cartItems])
+  const handleTotalPrice =() => {
+    // setTotalPrice(cartItems&& cartItems.reduce((acc,item)=>{
+    //   return acc + item.itemPrice;
+    // },0))
+    let item_price=0;
+    cartItems&& cartItems.forEach((item)=>{
+      // console.log(item.itemPrice);
+      item_price += item.itemPrice;
+    })
+    setTotalPrice(item_price)
+  }
   const getCartItems = ()=> {
-    axios.get(`http://127.0.0.1:8000/cart-api/6`)
+    axios.get(`http://127.0.0.1:8000/cart-api/5`)
     .then((response) =>{
       setCartItems(response.data[0].cartItems)
-      console.log(response.data)
+      // console.log(response.data)
     })
     .catch((error)=>console.log(error))
   }
+  const handleQuantity_increment = (item,itemID,itemQuantity)=> {
+    // console.log(itemID,itemQuantity);
+    // setQuantity(quantity+1)
+    setCartItems(item=>
+      item.map((item)=>
+      item.itemID === itemID ? {...item,itemQuantity:item.itemQuantity +1} :item
+      ))
+  }
+  const handleQuantity_decrement = (item,itemID,itemQuantity)=> {
+    // console.log(itemID,itemQuantity);
+    // setQuantity(quantity > 1? quantity - 1 : 1);  
+    setCartItems(item=>
+      item.map((item)=>
+      item.itemID === itemID ? {...item,itemQuantity:item.itemQuantity > 1 ? item.itemQuantity-1 :1} :item
+    ))
+    
+  }
+
   return (
     <>
       <div className='cart-container'>
@@ -47,8 +79,8 @@ function MyCart() {
                     <div className='quantity-div'>
                       <div className='calcDiv-container'>
                         <div className='calcDiv'>
-                          <button type='button'  className='calc-btn'><i className='pi pi-angle-up'></i></button>
-                          <button type='button'  className='calc-btn'><i className='pi pi-angle-down'></i></button>
+                          <button type='button'  className='calc-btn' onClick={()=>handleQuantity_increment(item,item.itemID,item.itemQuantity)}><i className='pi pi-angle-up'></i></button>
+                          <button type='button'  className='calc-btn' onClick={()=>handleQuantity_decrement(item,item.itemID,item.itemQuantity)}><i className='pi pi-angle-down'></i></button>
                         </div>
                         <div className='count'>{item.itemQuantity}</div>
                       </div>
@@ -86,7 +118,7 @@ function MyCart() {
               <div className="total-order">
                 <div className='col'>
                   <span className='order-total-price'>المجموع الكلي</span>
-                  <span className='order-totalPrice'>EGP  115</span>
+                  <span className='order-totalPrice'>EGP  {TotalPrice}</span>
                 </div>
               </div>
             </div>
